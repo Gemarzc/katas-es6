990b05580adeedaa87b274a25249febac45fcc83
Gemarzc <gemacarret31@gmail.com>
2024-08-30 16:59:19 +0200

    ejercicio 6.1

--- main.js
+++ main.js
@@ -1,50 +1,31 @@
-/* 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los valores que sean mayor que 18
-const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+/* 6.1 Dado el siguiente array, usa .find() para econtrar el número 100.
+const numbers = [32, 21, 63, 95, 100, 67, 43];
 
-5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los valores que sean par.
-const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
-
-5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los streamers que tengan el gameMorePlayed = 'League of Legends'.
-const streamers = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010.
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
 ];
 
-5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
-usar la funcion .includes() para la comprobación.
-const streamers = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 
+'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
+spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación 
+lo queremos meter en la propiedad .mutation del objeto fusionado.
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
 ];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir 
+	la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, 
+	solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un 
+	señor mayor como Personal Trainer'}
+]; */
 
-5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
-el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
-.includes() para la comprobación.
-Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
-.age sea mayor que 35. */
+const numbers = [32, 21, 63, 95, 100, 67, 43]
 
-const streamers = [
-  { name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft' },
-  { name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends' },
-  { name: 'Reven', age: 43, gameMorePlayed: 'League of Legends' },
-  { name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us' }
-]
-
-const filtered = streamers.filter((streamer) => {
-  if (streamer.gameMorePlayed.includes('Legends')) {
-    if (streamer.age > 35) {
-      streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase()
-    }
-    return streamer
-  }
-})
-
-console.log(filtered)
+console.log(numbers.find((number) => number === 100))
