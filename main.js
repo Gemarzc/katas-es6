5adc20d8876e34d8b269942afc28a8cbb15eb011
Gemarzc <gemacarret31@gmail.com>
2024-08-30 12:47:47 +0200

    ejercicio 4.2

--- main.js
+++ main.js
@@ -27,12 +27,19 @@ const cities = [
 	{isVisited:false, name: 'Seul'}
 ]; */
 
-const users = [
+const users2 = [
   { id: 1, name: 'Abel' },
   { id: 2, name: 'Julia' },
   { id: 3, name: 'Pedro' },
   { id: 4, name: 'Amanda' }
 ]
-const names = users.map((user) => user.name)
 
-console.log(names)
+const names2 = users2.map((user) => {
+  if (user.name[0] === 'A') {
+    return 'Anacleto'
+  } else {
+    return user.name
+  }
+})
+
+console.log(names2)
