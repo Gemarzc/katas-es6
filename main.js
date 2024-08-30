085687fb014861331f2a1ccdf31c4f53132c74e1
Gemarzc <gemacarret31@gmail.com>
2024-08-30 12:58:42 +0200

    ejercicio 5.3

--- main.js
+++ main.js
@@ -31,7 +31,15 @@ el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la func
 Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
 .age sea mayor que 35. */
 
-const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90]
-const filtered = ages.filter((age) => age % 2 === 0)
+const streamers = [
+  { name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft' },
+  { name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends' },
+  { name: 'Reven', age: 43, gameMorePlayed: 'League of Legends' },
+  { name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us' }
+]
+
+const filtered = streamers.filter(
+  (streamer) => streamer.gameMorePlayed === 'League of Legends'
+)
 
 console.log(filtered)
