2dd22497d381a71f2d7c80fb04a2417d3de1a639
Gemarzc <gemacarret31@gmail.com>
2024-09-02 11:15:32 +0200

    ejercicio 15

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-/* Crea una función llamada rollDice() que reciba como parametro el numero de caras 
-que queramos que tenga el dado que deberá simular el codigo dentro de la función. 
-Como hemos dicho, que la función use el parametro para simular una tirada de dado 
-y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te 
-preocupes! Busca información sobre la función de javascript Math.random() */
+/* Crea una función llamada swap que reciba un array y dos parametros que sean 
+indices del array. La función deberá intercambiar la posición de los valores de 
+los indices que hayamos enviado como parametro. Retorna el array resultante.
 
-const rollDice = (numFaces) => {
-  const randomNumber = Math.floor(Math.random() * numFaces) + 1
-  return randomNumber
+Sugerencia de array: */
+
+const fantasticFour = [
+  'La antorcha humana',
+  'Mr. Fantástico',
+  'La mujer invisible',
+  'La cosa'
+]
+
+const swap = (array, index1, index2) => {
+  const temp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = temp
+
+  return array
 }
 
-const numFaces = 6
-const result = rollDice(numFaces)
-console.log(`Has lanzado un dado de ${numFaces} caras y has sacado ${result}.`)
+const fantasticFourSwap = swap(fantasticFour, 2, 0)
+console.log(fantasticFourSwap)
