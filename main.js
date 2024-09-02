a3172a6a6b6e0be292789e5cf8543bde2a71f302
Gemarzc <gemacarret31@gmail.com>
2024-09-02 10:45:20 +0200

    ejercicio 11

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-/* Dado el siguiente javascript usa forof y forin para hacer la media del 
-volumen de todos los sonidos favoritos que tienen los usuarios. */
+/* Dado el siguiente javascript usa forof y forin para saber cuantas veces ha 
+sido cada sonido agregado por los usuarios a favorito.
 
+Para ello recorre la lista de usuarios y usa forin para recoger el nombre 
+de los sonidos que el usuario tenga como favoritos.
+
+Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada 
+vez que ese sonido se repita como favorito en cada usuario.
+
+Este ejercicio es un poco complicado con los conocimientos actuales pero...a 
+la vez un buen reto y oportunidad para comprender que hay muchas formas de 
+hacer las cosas en javascript. */
 const users = [
   {
     name: 'Alberto',
@@ -36,19 +45,17 @@ const users = [
   }
 ]
 
-let totalVolume = 0
-let soundCount = 0
+const soundCount = {}
 
 for (const user of users) {
   for (const sound in user.favoritesSounds) {
-    totalVolume += user.favoritesSounds[sound].volume
-    soundCount++
+    const soundName = sound
+    if (soundCount[soundName]) {
+      soundCount[soundName]++
+    } else {
+      soundCount[soundName] = 1
+    }
   }
 }
 
-const mediaVolume = totalVolume / soundCount
-
-console.log(
-  'La media de los sonidos favoritos de los usuarios es de',
-  mediaVolume
-)
+console.log(soundCount)
